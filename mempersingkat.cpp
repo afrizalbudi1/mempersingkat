cf6360c92b4a562166ca637478cc59eec927fe1b
afrzalbudi1 <32351812+afrizalbudi1@users.noreply.github.com>
2018-01-24 15:49:06 +0800

    kodingan selesai tugas siap dikumpuilkan

--- mempersingkat.cpp
+++ mempersingkat.cpp
@@ -16,4 +16,51 @@ int main(){
 	double hasil1,hasil2,hasil3,hasilkali,mc1,mc2,mc3,mc4,mc5,h1,h2,h3,h4,h5,h11;	
 		// ISI DATA YANG MANUAL PENCARIAN NILAI MAX
 		
+		mc1=max(4,max(3,5));
+		mc2=max(4,max(3,4));
+		mc3=max(5,max(4,2));
+		mc4=max(3,max(2,2));
+		mc5=max(3,max(3,2));
+		// PERHITUNGAN MANUAL HEHE
+		hasil1=4+3+4+4+2;
+		hasil2=3+2.25+3.2+2.6667+2;
+		hasil3=5+3+1.6+2.6667+1.3333;
+	// MENAMPILKAN NILAI
+	cout<<"MATRIKS X : "<<endl;
+	for(i=0;i<3;i++){
+		for (j=0;j<5;j++) {
+			cout<<ac[i][j]<<" | ";
 		}
+		cout<<endl;
+	}
+	
+	cout<<endl<<"MATRIKS R : "<<endl;
+	for(i=0;i<3;i++){
+		for (j=0;j<1;j++) { n=ac[i][j]; h1=n/mc1; cout<<h1<<" | "; }
+		for (j=1;j<2;j++) {	n=ac[i][j]; h2=n/mc2; cout<<h2<<" | "; }
+		for (j=2;j<3;j++) { n=ac[i][j]; h3=n/mc3; cout<<h3<<" | "; }
+		for (j=3;j<4;j++) {	n=ac[i][j]; h4=n/mc4; cout<<h4<<" | "; }
+		for (j=4;j<5;j++) { n=ac[i][j]; h5=n/mc5; cout<<h5<<" | "; }
+		cout<<endl;
+	}
+	
+	cout<<endl<<"BOBOT W : "<<endl;
+		for (j=0;j<5;j++) {
+			cout<<w[j]<<" | ";			
+}
+	cout<<endl;
+	
+	cout<<endl<<"MATRIKS R * W : "<<endl;
+	for(i=0;i<3;i++){
+		for (j=0;j<1;j++) { b=w[j]; n=ac[i][j]; h1=n/mc1; hasilkali=h1*b; cout<<hasilkali<<" | "; }
+		for (j=1;j<2;j++) { b=w[j]; n=ac[i][j]; h2=n/mc2; hasilkali=h2*b; cout<<hasilkali<<" | "; }
+		for (j=2;j<3;j++) { b=w[j]; n=ac[i][j]; h3=n/mc3; hasilkali=h3*b; cout<<hasilkali<<" | "; }
+		for (j=3;j<4;j++) { b=w[j]; n=ac[i][j]; h4=n/mc4; hasilkali=h4*b; cout<<hasilkali<<" | "; }
+		for (j=4;j<5;j++) { b=w[j]; n=ac[i][j]; h5=n/mc5; hasilkali=h5*b; cout<<hasilkali<<" | "; }
+		cout<<endl;
+}
+	cout<<endl<<"HASIL AKHIR : "<<endl;
+	cout<<"Nilai A1 : " <<hasil1<<endl;
+	cout<<"Nilai A2 : " <<hasil2<<endl;
+	cout<<"Nilai A3 : " <<hasil3<<endl;
+}
